df92868ccd9cd9b7e038e3785f11966de60bbe16
jackpreble <60149852+jackpreble@users.noreply.github.com>
2020-02-24 12:09:03 -0500

    Initial commit

---
+++ ball.py
@@ -0,0 +1,8 @@
+# class for the ball
+# https://www.101computing.net/pong-tutorial-using-pygame-adding-the-paddles/
+import pygame
+from random import randint
+black = (0,0,0)
+
+class Ball:
+    pass
